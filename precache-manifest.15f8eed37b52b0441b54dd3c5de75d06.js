8cd688669bf76af91774b54494d9e3fdd65817cb
Bogdan <bogdan.kharchykov@gmail.com>
2020-09-10 14:58:54 +0200

    Updates

---
+++ precache-manifest.15f8eed37b52b0441b54dd3c5de75d06.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "191d3ee2b3b2b6e840e92d6d40c95935",
+    "url": "/facerecognition/index.html"
+  },
+  {
+    "revision": "89d9e7c2d21464d58116",
+    "url": "/facerecognition/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "88fea2357c95c535970e",
+    "url": "/facerecognition/static/css/main.89f1ca78.chunk.css"
+  },
+  {
+    "revision": "89d9e7c2d21464d58116",
+    "url": "/facerecognition/static/js/2.e44985a2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/facerecognition/static/js/2.e44985a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88fea2357c95c535970e",
+    "url": "/facerecognition/static/js/main.05beceaf.chunk.js"
+  },
+  {
+    "revision": "fa6a11a9cd7eebd80802",
+    "url": "/facerecognition/static/js/runtime-main.1ad3eaac.js"
+  }
+]);
