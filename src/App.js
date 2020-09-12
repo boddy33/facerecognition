575ae8f7e28bc471d1d3f5399afdb5d9ca079a28
Bogdan <bogdan.kharchykov@gmail.com>
2020-09-12 14:00:09 +0200

    invalidating api key

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, {Component} from 'react';
 import './App.css';
-import Navigation from './components/navigation/Navigation';
 import Logo from './components/logo/Logo';
 import ImageLinkForm from './components/imagelinkform/ImageLinkForm';
 import Rank from './components/rank/Rank';
 import Clarifai from 'clarifai';
 import FaceRecognition from './components/facerecognition/FaceRecognition';
 
+const apiURL = 'http://localhost:3000';
+const apiMode = false;
+
 const app = new Clarifai.App(
   {
-    apiKey: '63e6be29c4c6495c9dca68ff2ceaba99'
+    apiKey: ''
   }
 );
 
@@ -24,7 +26,15 @@ class App extends Component {
     }
   }
 
-  calculateFaceLocation = (data) => {
+  componentDidMount() {
+    if (apiMode) {    
+      fetch(apiURL)
+      .then(response => response.json())
+      .then(console.log);
+    }
+  }
+
+ calculateFaceLocation = (data) => {
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
     const image = document.getElementById('inputimage');
     const width = Number(image.width);
@@ -51,6 +61,19 @@ class App extends Component {
       this.setState({imageUrl : this.state.input});
       app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
       .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .then(() => {
+        if (apiMode) {
+          fetch(apiURL + '/action-logs', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              name: "New URL Submitted",
+              message: "URL: " + this.state.input,
+              severity: "INFO"
+            })
+          }).catch(err => console.log);
+        }
+      })
       .catch(err => console.err(err));
   }
 
